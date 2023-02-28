3a7309842a5f3ac2724dc01bce249d66b44ece43
Vasiliy Kuzmin <vasilykuzmin22@gmail.com>
2023-02-28 20:58:52 +0300

    Added avx implementation

---
+++ main_avx.cpp
@@ -0,0 +1,38 @@
+#include "hash_map_avx.h"
+#include <iostream>
+#include <cstdlib>
+#include <stdexcept>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    HashMap<int, int> map;
+    using HashMapIterator = HashMap<int, int>::iterator;
+    int n;
+    std::cin >> n;
+    for (int i = 0; i < n; ++i) {
+        char code;
+        int key;
+        std::cin >> code;
+        if (code != '!' && code != '<')
+            std::cin >> key;
+        if (code == '-')
+            map.erase(key);
+        else if (code == '?') {
+            auto it = map.find(key);
+            std::cout << (it == map.end() ? -1 : it->second) << "\n";
+        } else if (code == '+') {
+            int val;
+            std::cin >> val;
+            map[key] = val;
+        } else if (code == '<') {
+            for(HashMapIterator it = map.begin(); it != map.end(); ++it)
+                std::cout << it->first << " " << it->second << "\n";
+        } else if (code == '!')
+            map.clear();
+    }
+
+    std::cout << map.size() << "\n";
+    return 0;
+}
--- makefile
+++ makefile
@@ -1,6 +1,6 @@
 CXX=g++
-CXXFLAGS=-fsanitize=address,undefined -fno-sanitize-recover=all -O2 -Wall -Werror -Wsign-compare
-
+CXXFLAGS=-std=c++17 -fsanitize=address,undefined -fno-sanitize-recover=all -O2 -Wall -Werror -Wsign-compare
+CXXAVXFLAGS=-march=native
 
 build_main:
 	$(CXX) $(CXXFLAGS) -o main main.cpp
@@ -9,12 +9,26 @@ main: build_main
 	./main
 	rm -f main
 
+build_main_avx:
+	$(CXX) $(CXXFLAGS) $(CXXAVXFLAGS) -o main_avx main_avx.cpp
+
+main_avx: build_main_avx
+	./main_avx
+	rm -f main_avx
+
 build_test:
 	$(CXX) $(CXXFLAGS) -o test test_hashmap.cpp
 
 test: build_test
 	./test
 	rm -f test
+
+build_test_avx:
+	$(CXX) $(CXXFLAGS) $(CXXAVXFLAGS) -o test_avx test_hashmap_avx.cpp
+
+test_avx: build_test_avx
+	./test_avx
+	rm -f test_avx
 
 clean:
 	rm -f main test
